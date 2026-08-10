7233bad54030e83b34151ba53775b0c956d3f02e
HFunction2013 <HFunction2013@outlook.com>
2026-08-10 16:47:01 +0800

    Add functions for `IN_CMD` and `INTERRUPTED`.

--- crates/cli-core/src/lib.rs
+++ crates/cli-core/src/lib.rs
@@ -1,4 +1,6 @@
-use std::sync::atomic::AtomicBool;
+use std::sync::atomic::{AtomicBool, Ordering};
+use ::safer_ffi::prelude::*;
+
 pub mod global_settings;
 pub mod print_content;
 pub mod commands;
@@ -8,3 +10,23 @@ pub mod result;
 
 pub static INTERRUPTED: AtomicBool = AtomicBool::new(false);
 pub static IN_CMD: AtomicBool = AtomicBool::new(false);
+
+#[ffi_export]
+pub fn is_interrupted() -> bool {
+    INTERRUPTED.load(Ordering::SeqCst)
+}
+
+#[ffi_export]
+pub fn is_in_cmd() -> bool {
+    IN_CMD.load(Ordering::SeqCst)
+}
+
+#[ffi_export]
+pub fn set_interrupted(val: bool) {
+    INTERRUPTED.store(val, Ordering::SeqCst);
+}
+
+#[ffi_export]
+pub fn set_in_cmd(val: bool) {
+    IN_CMD.store(val, Ordering::SeqCst);
+}
